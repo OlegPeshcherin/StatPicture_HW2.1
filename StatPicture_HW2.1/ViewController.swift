7f4d04a4b6972a288038d9a4cbfc7aa580646b7f
OlegPeshcherin <olegpnmsk@me.com>
2021-10-20 21:38:25 +0300

    Initial Commit

---
+++ StatPicture_HW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StatPicture_HW2.1
+//
+//  Created by Oleg Peshcherin on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
